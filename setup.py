850f6fe63d411d0a3c479af0804ed7bf1a39fa6f
JohanSRQ <jrincondev@gmail.com>
2025-10-17 12:22:42 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cuartoRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Johan Rincon",                          # Tu nombre
+    author_email="jrincon@ccxc.co",                 # Tu correo electrónico
+    url="https://github.com/jrinconccxc/miCuartoRepo",     # URL del proyecto
+)
